1f6f75e417f47feb9cedb66e3ed36b998fa66199
Ilya Bylich <ibylich@gmail.com>
2025-06-03 16:36:18 +0200

    enable custom logger for android build

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,21 @@ mod websocket;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn shared_clipboard_setup() {
+    #[cfg(target_os = "android")]
+    {
+        use android_logger::Config;
+        use log::LevelFilter;
+
+        android_logger::init_once(
+            Config::default()
+                .with_tag("RUST")
+                .with_max_level(LevelFilter::Trace),
+        );
+    }
+
+    #[cfg(not(target_os = "android"))]
     pretty_env_logger::init();
+
     log::info!("info example");
     log::error!("error example");
 }
