2c2ab24597b14c2bf24fa699eb5880e9f58be1f8
Ilya Bylich <ibylich@gmail.com>
2025-06-03 12:24:30 +0200

    add build option to use rustls

--- src/websocket.rs
+++ src/websocket.rs
@@ -16,10 +16,7 @@ impl Websocket {
 
         let connector = if is_wss {
             log::info!("wss protocol detected, enabling TLS");
-            Connector::NativeTls(tokio_native_tls::TlsConnector::from(
-                tokio_native_tls::native_tls::TlsConnector::new()
-                    .context("failed to create native TLS connector")?,
-            ))
+            ssl_connector()?
         } else {
             log::info!("plain ws protocol detected, disabling TLS");
             Connector::Plain
@@ -42,4 +39,40 @@ impl Websocket {
     pub(crate) async fn next(&mut self) -> Option<Result<Message, tokio_websockets::Error>> {
         self.ws.next().await
     }
+}
+
+#[cfg(feature = "native-tls")]
+fn ssl_connector() -> Result<Connector> {
+    Ok(Connector::NativeTls(tokio_native_tls::TlsConnector::from(
+        tokio_native_tls::native_tls::TlsConnector::new()
+            .context("failed to create native TLS connector")?,
+    )))
+}
+
+#[cfg(feature = "rustls-webpki-roots")]
+fn ssl_connector() -> Result<Connector> {
+    use rustls::ClientConfig;
+    use std::sync::Arc;
+    use tokio_rustls::TlsConnector;
+
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
+
+    let root_store = rustls::RootCertStore {
+        roots: webpki_roots::TLS_SERVER_ROOTS.to_vec(),
+    };
+    let config = ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    let connector = TlsConnector::from(Arc::new(config));
+
+    Ok(Connector::Rustls(connector))
+}
+
+#[cfg(all(not(feature = "native-tls"), not(feature = "rustls-webpki-roots")))]
+fn ssl_connector() -> Result<Connector> {
+    anyhow::bail!(
+        "either native-tls or rustls-webpki-roots feature must be enabled to run with SSL connector"
+    )
 }
